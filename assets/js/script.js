08e99768322b30d76530675e5a3f9f631076e9e5
Troy Johnson <troynjohnson6@gmail.com>
2023-01-05 00:46:44 -0800

    stuck refining date information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,243 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
 //$(function () {
-    // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. 
-    //HINT: What does `this` reference in the click listener function? 
-    //How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? 
-    //How might the id be useful when saving the description in local storage?
+// TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage.
+//HINT: What does `this` reference in the click listener function?
+//How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked?
+//How might the id be useful when saving the description in local storage?
 
+// TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour.
+//HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes?
+//How can Day.js be used to get the current hour in 24-hour time?
+
+// TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements.
+//HINT: How can the id attribute of each time-block be used to do this?
+
+// TODO: Add code to display the current date in the header of the page.
+// });
+myPlanner = new Object();
+
+function populatePlanner(i, meridian) {
+  var timeStr = i.toString().concat(meridian);
+
+  //create section
+  var secEl = $("<section>");
+  //create article
+  var artEl = $("<article>");
+  //create textarea
+  var taEl = $("<textarea>");
+  //create button
+  var btnEl = $("<button>");
+  var iconEl = $("<i>");
+
+  //append elements to dom
+  $("main").append(secEl);
+  secEl.append(artEl);
+  secEl.append(taEl);
+  secEl.append(btnEl);
+  btnEl.append(iconEl);
+
+  //set attributes
+  $("main").addClass("container-lg px-5");
+  secEl.attr("id", "hour-" + (i + 1)).addClass("row time-block");
+  artEl.addClass("col-2 col-md-1 hour text-center py-3").text(timeStr);
+  taEl.addClass("col-8 col-md-10 description").attr("rows", "3");
+  btnEl.addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save");
+  iconEl.addClass("fas fa-save").attr("aria-hidden", "true");
+
+  myPlanner[timeStr] && taEl.val(myPlanner[timeStr]);
+
+  //add event listener
+  btnEl.click(function () {
+    //save value to object
+    myPlanner[timeStr] = taEl.val();
+    //save object to local storage
+    localStorage.setItem("myPlannerStringify", JSON.stringify(myPlanner));
+  });
+}
+
+var today = new Date();
+var currentMonth = new String();
+var currentDay = new String();
+var currentHour = new String();
+var currentMinute = new String();
+var currentSecond = new String();
+var currentMeridian = new String();
+
+function setMonth() {
+  switch (today.getDay()) {
+    case 0:
+      currentMonth = "January";
+      break;
+    case 1:
+      currentMonth = "February";
+      break;
+    case 2:
+      currentMonth = "March";
+      break;
+    case 3:
+      currentMonth = "April";
+      break;
+    case 4:
+      currentMonth = "May";
+      break;
+    case 5:
+      currentMonth = "June";
+      break;
+    case 6:
+      currentMonth = "July";
+      break;
+    case 7:
+      currentMonth = "August";
+      break;
+    case 8:
+      currentMonth = "September";
+      break;
+    case 9:
+      currentMonth = "Ocbtober";
+      break;
+    case 10:
+      currentMonth = "November";
+      break;
+    case 11:
+      currentMonth = "December";
+      break;
+  }
+}
+
+function setDay() {
+  switch (today.getDay()) {
+    case 0:
+      currentDay = "Sunday";
+      break;
+    case 1:
+      currentDay = "Monday";
+      break;
+    case 2:
+      currentDay = "Tuesday";
+      break;
+    case 3:
+      currentDay = "Wednesday";
+      break;
+    case 4:
+      currentDay = "Thursday";
+      break;
+    case 5:
+      currentDay = "Friday";
+      break;
+    case 6:
+      currentDay = "Saturday";
+      break;
+  }
+}
+
+function setHour() {
+  if (today.getHours() === 0) {
+    currentHour = "12";
+    currentMeridian = "AM";
+  } else if (today.getHours() < 10) {
+    currentHour = today.getHours();
+    currentMeridian = "AM";
+  } else if (today.getHours() < 12) {
+    currentHour = today.getHours();
+    currentMeridian = "AM";
+  } else if (today.getHours() === 12) {
+    currentHour = "12:";
+    currentMeridian = "PM";
+  } else if (today.getHours() < 24) {
+    currentHour = today.getHours() - 12;
+    currentMeridian = "PM";
+  } else {
+    console.log("No Conditions Met");
+  }
+
+}
+
+function setMinute() {
+  if (today.getMinutes() < 10) {
+    currentMinute = today.getMinutes();
+  } else {
+    currentMinute = today.getMinutes();
+  }
+}
+
+function setSecond() {
+  if (today.getSeconds() < 10) {
+    currentSecond = today.getSeconds().toString();
+  } else {
+    currentSecond = today.getSeconds().toString();
+  }
+}
+
+function getDateWords() {
+  return (
+    currentDay +
+    ", " +
+    currentMonth +
+    " " +
+    today.getDate() +
+    ", " +
+    today.getFullYear()
+  );
+}
+
+function getDateNumbers() {
+  return (
+    today.getMonth() + 1 + "/" + today.getDate() + "/" + today.getFullYear()
+  );
+}
+
+function getTime() {
+  // console.log( currentHour)
+  // console.log( currentMinute)
+  // console.log( currentSecond)
+  return (
+    currentHour +
+    ":" +
+    currentMinute +
+    ":" +
+    currentSecond +
+    " " +
+    currentMeridian
+  );
+}
+
+function setToday() {
+  setMonth();
+  setDay();
+  setHour();
+  setMinute();
+  setSecond();
+}
+
+function displayDate() {
+    setToday()
+        $("#currentDay1").text(getDateWords());
+        $("#currentDay2").text(getDateWords() + "  " + getTime());
+        $("#currentDay3").text(getDateNumbers() + "  " + getTime());
+}
+function init() {
     
+    setInterval(function() {
+setToday();
+        displayDate();
+    }, 1000)
+  //planner ojbect empty
+  console.log(myPlanner);
+  //get item from LS, save to object
+  myPlanner = JSON.parse(localStorage.getItem("myPlannerStringify")) ?? {};
+  //planner object has data
+  console.log(myPlanner);
 
-    // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
-    //HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? 
-    //How can Day.js be used to get the current hour in 24-hour time?
+  var meridian = "";
+  for (var i = 9; i <= 12; i++) {
+    meridian = "am";
+    populatePlanner(i, meridian);
+  }
+  for (var i = 1; i <= 5; i++) {
+    meridian = "pm";
+    populatePlanner(i, meridian);
+  }
+}
 
-    // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. 
-    //HINT: How can the id attribute of each time-block be used to do this?
+init();
 
-    // TODO: Add code to display the current date in the header of the page.
- // });
-myPlanner = new Object
-
- function populatePlanner(i, meridian) {
-
-    var timeStr = i.toString().concat(meridian)
-    
-    //create section
-    var secEl = $('<section>');
-    //create article
-    var artEl = $('<article>');
-    //create textarea
-    var taEl = $('<textarea>');
-    //create button
-    var btnEl = $('<button>');
-    var iconEl = $('<i>');
-    
-    //append elements to dom
-    $('main').append(secEl);
-    secEl.append(artEl);
-    secEl.append(taEl);
-    secEl.append(btnEl);
-    btnEl.append(iconEl);
-
-
-    //set attributes
-    $('main').addClass('container-lg px-5')
-    secEl.attr('id', 'hour-' + (i + 1)).addClass('row time-block')
-    artEl.addClass('col-2 col-md-1 hour text-center py-3').text(timeStr)
-    taEl.addClass('col-8 col-md-10 description').attr('rows', '3')
-    btnEl.addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save')
-    iconEl.addClass('fas fa-save').attr('aria-hidden', 'true')
-
-    myPlanner[timeStr] && taEl.val(myPlanner[timeStr])
-
-    //add event listener
-    btnEl.click(function () {
-
-        //save value to object
-        myPlanner[timeStr] = taEl.val()
-        //save object to local storage
-        localStorage.setItem("myPlannerStringify", JSON.stringify(myPlanner));
-        
-    }
-    )
-    
- }
-    
- function init() {
-    //planner ojbect empty
-    console.log(myPlanner)
-    //get item from LS, save to object
-     myPlanner = JSON.parse(localStorage.getItem("myPlannerStringify")) ?? {}
-     //planner object has data
-     console.log(myPlanner)
-     
-    var meridian = "";
-    for (var i = 9; i <= 12; i++) {
-        meridian = "am"
- populatePlanner(i, meridian)
-    }
-        for (var i = 1; i <= 5; i++) {
-            meridian = "pm"
-     populatePlanner(i, meridian)
-        }
-
-    }
-
- init()
-
-
-  
